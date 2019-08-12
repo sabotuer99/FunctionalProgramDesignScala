9d3bc89910fcc45e3ffef8e904f80fd821863a78
troy <sabotuer99@gmail.com>
2019-08-11 18:20:19 -0700

    finished quickcheck assigmment, that was anticlimactic...

--- week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,27 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      a <- arbitrary[A]
+      h <- oneOf(const(empty), genHeap)
+    } yield insert(a, h))
+
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
+  property("gen1") = forAll { h: H =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+  property("sorted") = forAll { h: H =>
+    val testheap = meld(insert(3, empty), insert(1, insert(2, empty)))
+    val first = findMin(testheap)
+    val second = findMin(deleteMin(testheap))
+    val third = findMin(deleteMin(deleteMin(testheap)))
+    first == 1 && second == 2 && third == 3
   }
 
 }
